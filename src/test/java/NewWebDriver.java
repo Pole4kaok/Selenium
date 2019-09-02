822c967253d1ea4eca35b79ec48d2c5fe1a80f44
Polina <pole4kaok@yandex.ru>
2019-09-03 00:18:14 +0300

    Waiters added

--- src/test/java/NewWebDriver.java
+++ src/test/java/NewWebDriver.java
@@ -4,7 +4,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import  org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
 
@@ -13,11 +16,11 @@ public class NewWebDriver {
         System.setProperty("webdriver.chrome.driver", "D:\\ATM\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://qa-cms.cefile-app.com");
-        WebElement username = driver.findElement(By.xpath("//*[@id=\"loginForm\"]/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"));
+        WebElement username = driver.findElement(By.name("username"));
         username.sendKeys("opsmanager");
         WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("clerkfull");
-        WebElement loginBtn = driver.findElement(By.xpath("//*[@id=\"loginForm\"]/table/tbody/tr/td/div/table/tbody/tr/td/input"));
+        WebElement loginBtn = driver.findElement(By.xpath("//input[@value='Login']"));
         loginBtn.click();
 
        // Thread.sleep(2000);
@@ -34,6 +37,8 @@ public class NewWebDriver {
         WebElement typeAhead = driver.findElement(By.id("s2id_autogen1_search"));
         typeAhead.sendKeys("@@@");
         Thread.sleep(1000);
+       /* new WebDriverWait(driver,10)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"select2-results-1\"]")));*/
         WebElement dropdown = driver.findElement(By.xpath("//*[@id=\"select2-results-1\"]"));
         List<WebElement> options = dropdown.findElements(By.tagName("li"));
         for(WebElement option : options)
@@ -43,24 +48,18 @@ public class NewWebDriver {
                 break;
             }
         }
-              // WebElement randomClass = driver.findElement(By.id("select2-result-label-90"));
 
-       /* WebElement randomClass = driver.findElement(By.xpath("//*[@id=\"s2id_autogen1_search\"]"));
-        builder.moveToElement(randomClass).click(randomClass);
-        Action move = builder.build();
-        move.perform();*/
-
-        Thread.sleep(2000);
         WebElement saveBtn = driver.findElement(By.name("save"));
         saveBtn.click();
-        Thread.sleep(2000);
-
-        WebElement caseInfo = driver.findElement(By.xpath("//*[@id=\"leftNav\"]/div[1]/div/div/table/tbody/tr[1]/td[2]/table/tbody/tr/td[1]/span"));
+       // Thread.sleep(2000);
+        new WebDriverWait(driver, 10)
+                .until(ExpectedConditions.textToBePresentInElementLocated((By.xpath("//*[@id=\"caseRulesEnginePolling\"]/td/div/span[2]/span")),"Rules Engine Complete"));
+        WebElement caseInfo = driver.findElement(By.xpath("//td/span[@class='ct-content']"));
         String caseNumber = caseInfo.getText();
         //System.out.println(getCaseNumber);
         WebElement caseMenu = driver.findElement(By.id("menuItem-10000-0-main"));
         caseMenu.click();
-       // WebElement caseSearch = driver.findElement(By.xpath("//*[@id=\"menuItem-10053-0-sub\"]"));
+
         Thread.sleep(2000);
         WebElement caseSearch = driver.findElement(By.id("menuItem-10053-0-sub"));
         caseSearch.click();
@@ -68,14 +67,20 @@ public class NewWebDriver {
         WebElement caseNumberField = driver.findElement(By.xpath("//*[@id=\"caseSection\"]/table/tbody/tr[1]/td[2]/input"));
         caseNumberField.sendKeys(caseNumber);
 
-        WebElement searchBtn = driver.findElement(By.xpath("//*[@id=\"caseSearchForm\"]/table[3]/tbody/tr/td/input"));
+        WebElement searchBtn = driver.findElement(By.xpath("//input[@value='Search']"));
         searchBtn.click();
 
         Thread.sleep(2000);
         WebElement searchRslt = driver.findElement(By.xpath("//*[@id=\"caseSearchResultsTable\"]/table/tbody/tr/td[1]"));
         searchRslt.click();
 
+        WebElement foundCaseNumber = driver.findElement(By.xpath("//td/span[@class='ct-content']"));
+        String foundCase = foundCaseNumber.getText();
 
+        if(caseNumber.equals(foundCase))
+            System.out.println("Success");
+        else
+            System.exit(-1);
 
         driver.quit();
     }
